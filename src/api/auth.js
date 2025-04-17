1958e7b950584130af5fc45ea4dff41545caae88
bp_yskim3 <yskim@digicaps.com>
2025-04-17 11:17:13 +0900

    vite api url 수정정

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_AUTH_URL,
   headers: {
     "Content-Type": "application/json",
   },
